2947de98f8d424ee1a06a2a8db078a1bd58f582a
ElliottPohl <elliottpohl@gmail.com>
2018-09-24 00:18:12 -0700

    Add files via upload
    -Added progress bar
    -Now maintains input video codec for output instead of always using mp4

--- temporal_filter.py
+++ temporal_filter.py
@@ -1,47 +1,71 @@
 import cv2
 import numpy as np
 import argparse, os
+from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description='Statistical function across a number of video frames')
-parser.add_argument('-i','--input',required=True,help='Filepath for video input')
-parser.add_argument('-b','--buffer_size',help='Amount of frames to condense',default=5,type=int)
-parser.add_argument('-f','--filter',help='Filter type',choices=('mean','median','min','max'),default='median')
-args = vars(parser.parse_args())
+class TemporalFilter:
+    def __init__(self, buffer_size=15, filter_type='max'):
+        self.buffer_size = buffer_size
+        self.filter_type = self.setFilter(filter_type)
+        self.buffer_frames = []
 
-cap = cv2.VideoCapture(args['input'])
-fn_pre = args['input'].split('.')[:-1]
-fn = os.path.abspath(f"{'.'.join(fn_pre)}_{args['filter']}.mp4")
-h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-fps = cap.get(cv2.CAP_PROP_FPS)
-fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-out = cv2.VideoWriter(fn,fourcc,fps,(w,h))
-buffer = []
+    def update(self,img_input):
+        self.buffer_frames.append(img_input)
+        if len(self.buffer_frames) > self.buffer_size:
+            del self.buffer_frames[0]
 
-print(f"output:{fn} {w}x{h}@{fps}")
+    def setFilter(self,filter_string):
+        if filter_string.lower() == 'mean':
+            return np.mean
+        elif filter_string.lower() == 'median':
+            return np.median
+        elif filter_string.lower() == 'min':
+            return np.min
+        elif filter_string.lower() == 'max':
+            return np.max
 
-while True:
-    ret, frame = cap.read()
-    if ret:
-        buffer.append(frame)
-        if len(buffer) > args['buffer_size']:
-            del buffer[0]
+    def render(self):
+        return np.array(self.filter_type(np.array(self.buffer_frames), axis=0), np.uint8)
 
-        if args['filter'].lower() == 'median':
-            out_frame = np.array(np.median(np.array(buffer),0),np.uint8)
-        elif args['filter'].lower() == 'mean':
-            out_frame = np.array(np.mean(np.array(buffer),0),np.uint8)
-        elif args['filter'].lower() == 'min':
-            out_frame = np.array(np.min(np.array(buffer),0),np.uint8)
-        elif args['filter'].lower() == 'max':
-            out_frame = np.array(np.max(np.array(buffer),0),np.uint8)
-        else:
-            print('Filter type not recognized')
-            break
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Apply a statistical function across a number of video frames')
+    parser.add_argument('-i', '--input', required=True, help='Filepath for video input')
+    parser.add_argument('-b', '--buffer_size', help='Amount of frames included in filter', default=15, type=int)
+    parser.add_argument('-f', '--filter', help='Filter type', choices=('mean', 'median', 'min', 'max'), default='max')
+    args = vars(parser.parse_args())
 
-        out.write(out_frame)
-    else:
-        break
+    video_path = os.path.abspath(args['input'])
+    buffer_size = args['buffer_size']
+    filter_type = args['filter']
 
-out.release()
-cap.release()
+    cap = cv2.VideoCapture(video_path)
+
+    f_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    f_fps = cap.get(cv2.CAP_PROP_FPS)
+    f_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    f_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    f_fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+
+    vid_fp_rt, vid_fp_ext = os.path.split(video_path)
+    temp = vid_fp_ext.split('.')
+    out_fn = f"{'.'.join(temp[:-1])}_{filter_type}_{buffer_size}.{temp[-1]}"
+    out_fp = os.path.join(vid_fp_rt,out_fn)
+    print(out_fp)
+
+    out = cv2.VideoWriter(out_fp,f_fourcc,f_fps,(f_width,f_height))
+
+    tf = TemporalFilter(buffer_size=buffer_size,filter_type=filter_type)
+
+    for i in tqdm(range(f_count)):
+        ret, frame = cap.read()
+        if ret:
+            tf.update(frame)
+            img = tf.render()
+            out.write(img)
+
+    cap.release()
+    out.release()
+
+
+
+
